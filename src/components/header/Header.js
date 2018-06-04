3ceec295cc42c158fac793752bf0dec625f73ffc
jp-reynolds <jpreynolds215@gmail.com>
2018-06-04 16:20:32 -0600

    changed navbar to have hamburger drop down

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react';
 import { Navbar, NavItem, Nav } from 'react-bootstrap';
 import './Header.css';
 
-class Header extends Component {
-  render() {
-    return (
-    	<Navbar>
+   /* 	<Navbar>
 	  		<Navbar.Header>
     			<Navbar.Brand>
       				<a href="/"><i className="fas fa-paw"></i> Paw Prints</a>
@@ -25,7 +22,38 @@ class Header extends Component {
 	     			Camp
 	    		</NavItem>
 	  		</Nav>
-		</Navbar>
+		</Navbar>*/
+
+class Header extends Component {
+  render() {
+    return (
+
+
+<Navbar inverse collapseOnSelect>
+  		<Navbar.Header>
+    		<Navbar.Brand>
+      				<a href="/"><i className="fas fa-paw"></i> Paw Prints</a>
+    		</Navbar.Brand>
+    		<Navbar.Toggle />
+  		</Navbar.Header>
+
+  		<Navbar.Collapse>
+    	<Nav pullRight>
+      		<NavItem eventKey={1} href="/park">
+	  	  			<p className="glyphicon glyphicon-tree-deciduous"></p>
+	      			Park
+	    		</NavItem>
+	    		<NavItem eventKey={2} href="/hike">
+	    			<p className="glyphicon glyphicon-tree-conifer"></p>
+	     			Hike
+	    		</NavItem>
+	    		<NavItem eventKey={3} href="/camp">
+	    			<p className="glyphicon glyphicon-tent"></p>
+	     			Camp
+	    		</NavItem>
+    	</Nav>
+  		</Navbar.Collapse>
+</Navbar>
     );
   }
 }
